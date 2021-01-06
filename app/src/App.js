71df76d48cd8e9fdaa02bb697ba3020194b4b9bb
chqiu6 <chucky-qiu@lambdastudents.com>
2021-01-05 19:14:57 -0500

    i somehow got it to send back our ghibi movies, updating our fix that we forgot to do

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import GhibiMovie from "./components/GhibiMovie";
-// import {connect} from "react-redux";
-// import {getGhibiApi} from "./actions/action";
+import {connect} from "react-redux";
+import {getGhibiApi} from "./actions/action";
 import axios from "axios";
 
-function App() {
+function App({loading, ghibiList, errors, getGhibiApi}) {
   //testing our api link 
   // axios
   // .get("https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe")
@@ -17,7 +17,7 @@ function App() {
   // })
   return (
     <div className="App">
-      <p> hi </p>
+      <p> Ghibi Movies List </p>
       <GhibiMovie />
     </div>
   );
--- app/src/components/GhibiMovie.js
+++ app/src/components/GhibiMovie.js
@@ -1,27 +1,34 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {connect} from "react-redux";
 import {getGhibiApi} from "../actions/action";
 
-const GhibiMovie = props =>{ 
+const GhibiMovie = (props) =>{ 
+    useEffect(() => {
+        props.getGhibiApi();
+    },[]);
+    // const [movieTest, setMovieTest] = useState([]);
+    // setMovieTest(ghibiList);
     console.log("What props are we receiving here? : ", props);
+    // console.log("is movietest working", movieTest);
     return(
         <div className = "ghibi-movie-list">
-            <h1>testing :</h1>
-            {/* mapping */}
-            {/* {props.ghibiList.map(item => {
-                return(
-                    <div className = "ghibi-movie">
-                        <h2>{props.item.title}</h2>
-                    </div>
-                )
-            })} */}
+            {/* <h1>testing :</h1> */}
+            
              {
-                props.ghibiList.map((item) => (
-                    <div className = "ghibi-movie">
-                        <p>{item.title}</p> 
+                props.ghibiList.map((item,i) => (
+                    <div className = "ghibi-movie" key = {item.id}>
+                        <p>#{i + 1}</p>
+                        <p>Title : {item.title}</p> 
+                        <p>Description : {item.description}</p>
+                        <p>Director : {item.director}</p>
+                        <p>Producer : {item.producer}</p>
+                        <p>Release Date : {item.release_date}</p>
+                        <p>RT Score : {item.rt_score}</p>
                     </div>
                 ))
             }
+            {/* <button onClick = {getGhibiApi}>Button action</button> */}
+            {/* <button onClick = {() => {setMovieTest(ghibiList);}}>Button</button> */}
         </div>
     )
 }
